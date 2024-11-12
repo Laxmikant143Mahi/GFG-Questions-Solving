a5c8607444d3d72abe074915d53adc9913a1df63
Laxmikant Mahindrakar <111036655+Laxmikant143Mahi@users.noreply.github.com>
2024-11-12 13:58:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Meeting Rooms/meeting-rooms.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] arr = new int[n][2];
+            for (int i = 0; i < n; i++) {
+                String temp[] = br.readLine().trim().split(" ");
+                arr[i][0] = Integer.parseInt(temp[0]);
+                String x = temp[1];
+                arr[i][1] = Integer.parseInt(x);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.canAttend(arr);
+            if (ans)
+                System.out.println("true");
+            else
+                System.out.println("false");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    static boolean canAttend(int[][] arr) {
+     
+     
+     Arrays.sort(arr,Comparator.comparingInt(o->o[1]));
+     
+     
+     int lastEnd=arr[0][1];
+     
+     for(int i=1;i<arr.length;i++){
+         
+         if(arr[i][0]<lastEnd){
+             return false;
+         }
+         
+         lastEnd=arr[i][1];
+     }
+     
+     return true;
+     
+    }
+}
