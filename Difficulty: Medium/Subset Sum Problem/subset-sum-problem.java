4bc8df5b214af7b0705fc8744a98ceb9b4d67061
Laxmikant Mahindrakar <111036655+Laxmikant143Mahi@users.noreply.github.com>
2024-12-25 10:16:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int N = input_line.length;
+            int arr[] = new int[N];
+            for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(input_line[i]);
+            int sum = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            if (ob.isSubsetSum(arr, sum))
+                System.out.println("true");
+            else
+                System.out.println("false");
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+
+    static Boolean isSubsetSum(int arr[], int target) {
+       int n = arr.length;
+
+        // DP table where dp[i][j] is true if a subset of the first i elements has a sum of j
+        boolean[][] dp = new boolean[n + 1][target + 1];
+
+        // If the target is 0, it is always possible to achieve (empty subset)
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
+
+        // Fill the DP table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= target; j++) {
+                // Exclude the current element
+                dp[i][j] = dp[i - 1][j];
+
+                // Include the current element if it does not exceed the current target
+                if (j >= arr[i - 1]) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j - arr[i - 1]];
+                }
+            }
+        }
+
+        // The answer is whether we can achieve the target with all elements
+        return dp[n][target];
+    }
+}
