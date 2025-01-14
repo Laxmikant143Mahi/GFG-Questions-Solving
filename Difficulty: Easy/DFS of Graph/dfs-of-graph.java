d6d273ab384d60d603a74906a24495ce9cbe6832
Laxmikant Mahindrakar <111036655+Laxmikant143Mahi@users.noreply.github.com>
2025-01-14 07:26:46 +0300

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(adj);
+            for (int i = 0; i < ans.size(); i++) System.out.print(ans.get(i) + " ");
+            System.out.println();
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(ArrayList<ArrayList<Integer>> adj) {
+    int V = adj.size(); // Get the number of vertices
+    boolean[] visited = new boolean[V];
+    ArrayList<Integer> ans = new ArrayList<>();
+
+    // Start DFS from node 0
+    DFS(0, adj, ans, visited);
+
+    return ans;
+}
+
+    private void DFS(int node, ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> ans, boolean[] visited) {
+        visited[node] = true;
+        ans.add(node);
+    
+        for (int neighbor : adj.get(node)) {
+            if (!visited[neighbor]) {
+                DFS(neighbor, adj, ans, visited);
+            }
+        }
+    }
+
+}
