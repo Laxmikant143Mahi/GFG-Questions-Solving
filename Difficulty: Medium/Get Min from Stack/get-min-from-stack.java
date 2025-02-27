50724f13fbd272cd9d4959eb40d086e6de03ff28
Laxmikant Mahindrakar <111036655+Laxmikant143Mahi@users.noreply.github.com>
2025-02-27 07:25:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Get Min from Stack/get-min-from-stack.java
+++ Difficulty: Medium/Get Min from Stack/get-min-from-stack.java
@@ -1,91 +1,75 @@
 //{ Driver Code Starts
 import java.util.*;
 
+class Get_Min_From_Stack {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); // Number of test cases
 
+        while (T-- > 0) {
+            int q = sc.nextInt(); // Number of queries
+            Solution g = new Solution();
 
-class Get_Min_From_Stack
-{
-	public static void main(String args[])
-	{
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		while(T>0)
-		{
-			int q = sc.nextInt();
-			GfG g = new GfG();
-			while(q>0)
-			{
-				int qt = sc.nextInt();
-				
-				if(qt == 1)
-				{
-					int att = sc.nextInt();
-					g.push(att);
-					//System.out.println(att);
-				}
-				else if(qt == 2)
-				{
-					System.out.print(g.pop()+" ");
-				}
-				else if(qt == 3)
-				{
-					System.out.print(g.getMin()+" ");
-				}
-			
-			q--;
-			}
-			System.out.println();
-		T--;
-		
-System.out.println("~");
+            while (q-- > 0) {
+                int qt = sc.nextInt();
+
+                if (qt == 1) {
+                    int att = sc.nextInt();
+                    g.push(att);
+                } else if (qt == 2) {
+                    g.pop(); // Just call pop(), do not print anything
+                } else if (qt == 3) {
+                    System.out.print(g.peek() + " "); // Print top element
+                } else if (qt == 4) {
+                    System.out.print(g.getMin() + " "); // Print minimum element
+                }
+            }
+            System.out.println("\n~");
+        }
+        sc.close();
+    }
 }
-		
-	}
-}
-
-
 
 // } Driver Code Ends
 
 
+class Solution {
+    private class Pair{
+        int val;
+        int min;
+        Pair(int val, int min){
+            this.val = val;
+            this.min = min;
+        }
+    }
+    public Solution() {}
 
-
-class GfG {
-    int minEle;
-    Stack<Integer> s;
-    Stack<Integer> minStack;
-
-    // Constructor    
-    GfG() {
-        s = new Stack<>();
-        minStack = new Stack<>();
+    Stack<Pair> minStack = new Stack<>();
+    
+    public void push(int x) {
+        if(minStack.isEmpty()){
+            minStack.push(new Pair(x, x));
+        }else{
+            minStack.push(new Pair(x, Math.min(x, minStack.peek().min)));
+        }
+        
     }
 
-    /* Returns min element from stack */
-    int getMin() {
-        if (minStack.isEmpty()) {
-            return -1; // Stack is empty
-        }
-        return minStack.peek();
+    // Remove the top element from the Stack
+    public void pop() {
+        if(minStack.isEmpty()) return;
+        minStack.pop();
     }
 
-    /* Returns popped element from stack */
-    int pop() {
-        if (s.isEmpty()) {
-            return -1; // Stack is empty
-        }
-        int popped = s.pop();
-        if (popped == minStack.peek()) {
-            minStack.pop(); // Remove from minStack if it's the minimum
-        }
-        return popped;
+    // Returns top element of the Stack
+    public int peek() {
+        if(minStack.isEmpty()) return -1;
+        return minStack.peek().val;
     }
 
-    /* Push element x into the stack */
-    void push(int x) {
-        s.push(x);
-        if (minStack.isEmpty() || x <= minStack.peek()) {
-            minStack.push(x); // Update minStack if x is a new minimum
-        }
+    // Finds minimum element of Stack
+    public int getMin() {
+        if(minStack.isEmpty()) return -1;
+        return minStack.peek().min;
     }
 }
